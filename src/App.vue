a8496da66b080178b48cbed199e4612c9cda2aba
Farrukh <farrukhx50@gmail.com>
2022-11-26 20:59:38 +0500

    feat: add new component

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,67 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import Information from "./components/Infromation.vue";
+
+const list = [
+  {
+    subTitle: "Ваш тариф",
+    title: "Биё! Фаъол бош - Сотрудник",
+    info: "Сменить тариф",
+  },
+  {
+    subTitle: "Остатки минут по РТ:",
+    title: "102",
+    info: "Подключай минуты +",
+  },
+  {
+    subTitle: "Остатки интернет трафика:",
+    title: "987 МБ",
+    info: "Пополнить мегабайты +",
+  },
+  {
+    subTitle: "Остатки бонусных баллов:",
+    title: "150",
+    info: "Обменять",
+  },
+];
 </script>
 
 <template>
-  <div class="overflow-x-hidden">
-    <div class="w-full h-44 bg-[#00bd49] flex justify-between">
-      <img class="pl-24 pb-14" src="./assets/megafon--1.svg" alt="logo" />
-      <div>
-        <div class="relative">
-          <p class="text-white pr-10 pt-7">+992 90 000 00</p>
-          <img
-            class="absolute left-32 top-6"
-            src="./assets/icons8-sort-down-30.png"
-            width="20"
+  <div class="h-screen overflow-x-hidden">
+    <header class="w-full h-44 bg-[#00bd49]">
+      <div class="max-w-[1200px] flex justify-between w-full m-auto pt-[20px]">
+        <img src="./assets/megafon--1-cropped.svg" width="120" alt="logo" />
+        <div>
+          <div class="flex items-center">
+            <p class="text-white">
+              <a href="tel:+9929000000">+992 90 000 00</a>
+            </p>
+            <img src="./assets/icons8-sort-down-30.png" width="20" />
+          </div>
+          <p class="text-white">Фамилия Имя Отчество</p>
+        </div>
+      </div>
+    </header>
+    <main class="w-screen">
+      <section
+        class="max-w-[1200px] w-full relative -top-8 flex justify-between m-auto"
+      >
+        <div class="bg-white w-1/3 h-36 rounded-xl">
+          <Information
+            title="12,00 TJS"
+            subTitle="Текущий баланс:"
+            info="+ Пополнить"
           />
         </div>
-        <p class="text-white pr-10">Фамилия Имя Отчество</p>
-      </div>
-    </div>
-    <div class="bg-[#ececec] w-screen h-screen relative">
-      <div class="bg-white w-96 h-36 rounded-xl absolute left-28 -top-8">
-        <p class="text-gray-400 pt-6 pl-6">Текущий баланс:</p>
-        <h4 class="pt-2 pl-6 text-3xl"><strong>12,00 TJS</strong></h4>
-        <p class="pt-1 pl-6 text-blue-400"><strong>+ Пополнить</strong></p>
-      </div>
-      <div class="bg-white w-[600px] rounded-xl absolute left-[510px] -top-8">
-        <div>
-          <p class="text-gray-400 pt-6 pl-6">Ваш тариф:</p>
-          <h4 class="pt-2 pl-6 text-3xl">
-            <strong>Биё! Фаъол бош - Сотрудник</strong>
-          </h4>
-          <p class="pt-1 pl-6 text-blue-400"><strong>Сменить тариф</strong></p>
-        </div>
-        <div>
-          <p class="text-gray-400 pt-6 pl-6">Остатки минут по РТ:</p>
-          <h4 class="pt-2 pl-6 text-3xl">
-            <strong>102 минуты</strong>
-          </h4>
-          <p class="pt-1 pl-6 text-blue-400">
-            <strong>Подключай минуты +</strong>
-          </p>
-        </div>
-        <div>
-          <p class="text-gray-400 pt-6 pl-6">Остатки интернет трафика:</p>
-          <h4 class="pt-2 pl-6 text-3xl">
-            <strong>987 МБ</strong>
-          </h4>
-          <p class="pt-1 pl-6 text-blue-400">
-            <strong>Подключай мегабайты +</strong>
-          </p>
-        </div>
-        <div>
-          <p class="text-gray-400 pt-6 pl-6">Остатки бонусных баллов:</p>
-          <h4 class="pt-2 pl-6 text-3xl">
-            <strong>150</strong>
-          </h4>
-          <p class="pt-1 pl-6 text-blue-400"><strong>Обменять</strong></p>
-        </div>
-      </div>
-    </div>
+        <ul class="bg-white rounded-xl w-[65%] pb-[40px]">
+          <li v-for="item in list" :key="item.title">
+            <Information
+              :title="item.title"
+              :subTitle="item.subTitle"
+              :info="item.info"
+            />
+          </li>
+        </ul>
+      </section>
+    </main>
   </div>
 </template>
---
+++ src/components/Infromation.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <p class="text-gray-400 pt-6 pl-6">{{ subTitle }}</p>
+    <h2 class="pt-2 pl-6 text-3xl">
+      <strong>{{ title }}</strong>
+    </h2>
+    <a class="pt-1 pl-6 text-blue-400" href=""
+      ><strong>{{ info }}</strong></a
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Information",
+  props: {
+    subTitle: String,
+    title: String,
+    info: String,
+  },
+  setup(props) {
+    console.log(props.title);
+  },
+};
+</script>
